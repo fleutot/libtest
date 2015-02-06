14759a91917549e02e16d5197b1aff410fb6ddcb
Gauthier Östervall <gauthier@ostervall.se>
2015-02-06 19:31:53 +0100

    Add library foo

---
+++ foo/foo.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "foo.h"
+
+void foodo(void)
+{
+    printf("In foo\n");
+}
---
+++ foo/foo.h
@@ -0,0 +1,6 @@
+#ifndef FOO_H_INCLUDED
+#define FOO_H_INCLUDED
+
+void foodo(void);
+
+#endif // FOO_H_INCLUDED
